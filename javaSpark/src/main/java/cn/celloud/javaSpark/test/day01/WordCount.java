a8fa5ed301c17119593848d3b37541f4e935d12a
gaotianye <15156506755@163.com>
2017-03-29 17:25:10 +0800

    update：set JVM参数：spark.testing.memory 为2G

--- javaSpark/src/main/java/cn/celloud/javaSpark/test/day01/WordCount.java
+++ javaSpark/src/main/java/cn/celloud/javaSpark/test/day01/WordCount.java
@@ -1,6 +1,8 @@
 package cn.celloud.javaSpark.test.day01;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -12,26 +14,32 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.VoidFunction;
 
 import scala.Tuple2;
+
 /**
  * 普通的wordcount，通过eclipse展示，输入文件是本地磁盘，输出在控制台上
+ * 
  * @author Administrator
  *
  */
 public class WordCount {
 	public static void main(String[] args) {
-		SparkConf conf = new SparkConf().setMaster("local[1]").setAppName("wordcount-java-localdisk");
+		//给JVM足够的资源，目前提供2G
+		SparkConf conf = new SparkConf().setMaster("local[3]").setAppName("wordcount-java-localdisk")
+				.set("spark.testing.memory", "2147480000");
+		;
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		/**
-		 * gao tian ye
-		 * hello world
+		 * gao tian ye hello world
 		 */
-		JavaRDD<String> linesRDD = sc.textFile("E:/test/2017-03-10");
+		List list = new ArrayList();
+		/*
+		 * list.add("my"); list.add("name"); list.add("my"); list.add("name");
+		 * list.add("my"); list.add("name");
+		 */
+		JavaRDD<String> linesRDD = sc.textFile("file:///E:/test/a.txt");
+		// JavaRDD linesRDD = sc.parallelize(list);
 		/**
-		 * gao
-		 * tian
-		 * ye
-		 * hello
-		 * world
+		 * gao tian ye hello world
 		 */
 		JavaRDD<String> wordsRDD = linesRDD.flatMap(new FlatMapFunction<String, String>() {
 			private static final long serialVersionUID = 1L;
@@ -41,11 +49,7 @@ public class WordCount {
 			}
 		});
 		/**
-		 * gao 1
-		 * tian 1
-		 * ye 1
-		 * hello 1
-		 * world 1
+		 * gao 1 tian 1 ye 1 hello 1 world 1
 		 */
 		JavaPairRDD<String, Integer> wordRDD = wordsRDD.mapToPair(new PairFunction<String, String, Integer>() {
 			private static final long serialVersionUID = 1L;
@@ -55,11 +59,7 @@ public class WordCount {
 			}
 		});
 		/**
-		 * gao 1
-		 * tian 1
-		 * ye 1
-		 * hello 1
-		 * world 1
+		 * gao 1 tian 1 ye 1 hello 1 world 1
 		 */
 		JavaPairRDD<String, Integer> resultRDD = wordRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
 			private static final long serialVersionUID = 1L;
@@ -68,12 +68,12 @@ public class WordCount {
 				return v1 + v2;
 			}
 		});
-		//控制台上展示
-		resultRDD.foreach(new VoidFunction<Tuple2<String,Integer>>() {
+		// 控制台上展示
+		resultRDD.foreach(new VoidFunction<Tuple2<String, Integer>>() {
 			private static final long serialVersionUID = 1L;
 
 			public void call(Tuple2<String, Integer> result) throws Exception {
-				System.out.println("word is :"+result._1+"  count is :"+result._2);
+				System.out.println("word is :" + result._1 + "  count is :" + result._2);
 			}
 		});
 	}
