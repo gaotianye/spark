745a175ac3a1f27b856719edaf7e87d708a5f129
gaotianye <15156506755@163.com>
2017-05-16 14:03:30 +0800

    update：更新

--- javaSpark/src/main/java/cn/celloud/javaSpark/test/day01/WordCount.java
+++ javaSpark/src/main/java/cn/celloud/javaSpark/test/day01/WordCount.java
@@ -1,8 +1,6 @@
 package cn.celloud.javaSpark.test.day01;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
